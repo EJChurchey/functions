be5dbbf2f2a79832a8f81554a9cba232381498c2
Your Name <everettchurchey@crbc.edu>
2015-04-07 10:05:50 -0400

    functions

--- functions/main.m
+++ functions/main.m
@@ -7,11 +7,21 @@
 //
 
 #import <Foundation/Foundation.h>
-
+int Square (int value){
+    return value * value;
+}
+int AddInt (int value, int value2, int value3){
+    return value + value2 + value3;
+    
+}
+float AddFloat (float value, float value2, float value3){
+    return value + value2 + value3;
+    
+}
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+        NSLog(@"%f",AddFloat (10.5,5.5,20.5));
     }
     return 0;
 }
